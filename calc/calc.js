ee4a5f8c091d3ca67cd562a75540020ba4fcd208
srijana2783 <97929892+srijana2783@users.noreply.github.com>
2022-12-21 16:00:41 -0500

    Add files via upload
    Added Calculator using Java script and Dice game too.

---
+++ calc/calc.js
@@ -0,0 +1,29 @@
+// var firstNum = 0;
+// var secNum = 0;
+// var result = 0;
+// function readFN(){
+//     firstNum = parseInt(document.getElementById("fn").value); 
+// }
+// function readSN(){
+//     secNum = parseInt(document.getElementById("sn").value); 
+// }
+// function reslt(rsValue){
+//     if(rsValue == 'addition'){
+//         result = firstNum+secNum;
+//     }
+//     if(rsValue == 'substract'){
+//         result = firstNum-secNum;
+//     }
+//     if(rsValue == 'mult'){
+//         result = firstNum*secNum;
+//     }
+//     if(rsValue == 'divide'){
+//         result = firstNum/secNum;
+//     }
+//  document.getElementById("rs").value = result;
+// }
+
+const age = [];
+ var ages = this.age >18 ? 'you can vote' : 'you cannot vote';
+ console.log(ages);
+
